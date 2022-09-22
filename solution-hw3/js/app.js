e373609969f22e42eb856e0c7a441d3e1c9e1c8d
jonathanho168 <62679754+jonathanho168@users.noreply.github.com>
2022-09-22 12:38:00 -0400

    add app.js to compute price of items per glazing, pack size

---
+++ solution-hw3/js/app.js
@@ -0,0 +1,90 @@
+
+// list of glazing options with price adaptation
+let glazing = [
+    {
+        glazing: 'Keep original',
+        price: 0.0,
+    },
+    {
+        glazing: 'Sugar milk',
+        price: 0.0,
+    },
+    {
+        glazing: 'Vanilla milk',
+        price: 0.5,
+    },
+    {
+        glazing: 'Double chocolate',
+        price: 1.50,
+    }
+]
+
+// list of pack size options with price adaptation
+let packsize = [
+    {
+        size: 1,
+        priceFactor: 1,
+    },
+    {
+        size: 3,
+        priceFactor: 3,
+    },
+    {
+        size: 6,
+        priceFactor: 5,
+    },
+    {
+        size: 12,
+        priceFactor: 10,
+    }
+]
+
+//populate the dropdowns with the above
+let glazeDropdown = document.querySelector('#glazing');
+let packDropdown = document.querySelector('#packsize');
+
+for (var i = 0; i < glazing.length; i++) {
+    var option = glazing[i];
+    var optionElement = document.createElement("option");
+    optionElement.textContent = option.glazing;
+    optionElement.value = option.price;
+    glazeDropdown.appendChild(optionElement);    
+}
+
+for (var i = 0; i < packsize.length; i++) {
+    var option = packsize[i];
+    var optionElement = document.createElement("option");
+    optionElement.textContent = option.size;
+    optionElement.value = option.priceFactor;
+    packDropdown.appendChild(optionElement);    
+}
+
+
+
+// Update price based on customer selection
+let glazingChange = document.querySelector('#glazing');
+glazingChange.addEventListener("change", onSelectChange);
+
+let packChange = document.querySelector('#packsize');
+packChange.addEventListener("change", onSelectChange);
+
+function onSelectChange() {
+    console.log("onSelectChange");
+
+
+    let glazeAdapt = Number(glazingChange.value);
+    let packAdapt = Number(packChange.value);
+
+    // get the price element so that we have something to modify
+    let price = document.querySelector('#calc-price');
+
+    // calculate final price of the product to be added to cart
+    const basePrice = 2.49;
+    let calculatedPrice_temp = (basePrice + glazeAdapt) * packAdapt * 100;
+    let calculatedPrice_rounded = Math.round(calculatedPrice_temp) / 100;
+    let calculatedPrice= ("$" + calculatedPrice_rounded);
+
+    // modify webpage HTML
+    price.innerText = calculatedPrice;
+    
+}
