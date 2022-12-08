878b639fdcff5e9e8012b0b462429310ebbe1026
jonathanho168 <62679754+jonathanho168@users.noreply.github.com>
2022-12-08 18:43:27 -0500

    popper

--- portfolio/js/about.js
+++ portfolio/js/about.js
@@ -1,17 +1,22 @@
-import { createPopper } from '@popperjs/core';
-import './styles.css';
+// import { createPopper } from '@popperjs/core';
+// import './styles.css';
 
-const popcorn = document.querySelector('#taipei-101');
+// const popcorn = document.querySelector('#taipei-101');
+// const tooltip = document.querySelector('#tooltip');
+
+// createPopper(popcorn, tooltip, {
+//   placement: 'top',
+//   modifiers: [
+//     {
+//       name: 'offset',
+//       options: {
+//         offset: [0, 8],
+//       },
+//     },
+//   ],
+// });
+
+
+const taipei101 = document.querySelector('#taipei-101');
 const tooltip = document.querySelector('#tooltip');
-
-createPopper(popcorn, tooltip, {
-  placement: 'top',
-  modifiers: [
-    {
-      name: 'offset',
-      options: {
-        offset: [0, 8],
-      },
-    },
-  ],
-});
+const popperInstance = Popper.createPopper(taipei101, tooltip);
