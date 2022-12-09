2266eaf1eba3d554a51f9928aa60f7a69d41a684
jonathanho168 <62679754+jonathanho168@users.noreply.github.com>
2022-12-08 23:58:45 -0500

    dynamic resizing, popper example

--- portfolio/js/about.js
+++ portfolio/js/about.js
@@ -1,55 +1,54 @@
 const button = document.querySelector('#button');
-      const tooltip = document.querySelector('#tooltip');
+const tooltip = document.querySelector('#tooltip');
 
-      const popperInstance = Popper.createPopper(button, tooltip, {
-        modifiers: [
-          {
+const popperInstance = Popper.createPopper(button, tooltip, {
+    modifiers: [{
             name: 'offset',
             options: {
               offset: [0, 8],
             },
           },
         ],
-      });
+});
 
-      function show() {
-        // Make the tooltip visible
-        tooltip.setAttribute('data-show', '');
+function show() {
+    // Make the tooltip visible
+    tooltip.setAttribute('data-show', '');
 
-        // Enable the event listeners
-        popperInstance.setOptions((options) => ({
-          ...options,
-          modifiers: [
-            ...options.modifiers,
-            { name: 'eventListeners', enabled: true },
-          ],
-        }));
+    // Enable the event listeners
+    popperInstance.setOptions((options) => ({
+        ...options,
+        modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: true },
+        ],
+    }));
 
-        // Update its position
-        popperInstance.update();
-      }
+    // Update its position
+    popperInstance.update();
+}
 
-      function hide() {
-        // Hide the tooltip
-        tooltip.removeAttribute('data-show');
+function hide() {
+    // Hide the tooltip
+    tooltip.removeAttribute('data-show');
 
-        // Disable the event listeners
-        popperInstance.setOptions((options) => ({
-          ...options,
-          modifiers: [
-            ...options.modifiers,
-            { name: 'eventListeners', enabled: false },
-          ],
-        }));
-      }
+    // Disable the event listeners
+    popperInstance.setOptions((options) => ({
+        ...options,
+        modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: false },
+        ],
+    }));
+}
 
-      const showEvents = ['mouseenter', 'focus'];
-      const hideEvents = ['mouseleave', 'blur'];
+const showEvents = ['mouseenter', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
 
-      showEvents.forEach((event) => {
-        button.addEventListener(event, show);
-      });
+showEvents.forEach((event) => {
+    button.addEventListener(event, show);
+});
 
-      hideEvents.forEach((event) => {
-        button.addEventListener(event, hide);
-      });
+hideEvents.forEach((event) => {
+    button.addEventListener(event, hide);
+});
